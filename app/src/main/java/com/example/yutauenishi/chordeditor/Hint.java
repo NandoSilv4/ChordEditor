353b229df8cc78d461cbffef017d352763ecb380
YutaUenishi <yuta915u@gmail.com>
2017-10-11 11:07:13 +0900

    test

--- app/src/main/java/com/example/yutauenishi/chordeditor/Hint.java
+++ app/src/main/java/com/example/yutauenishi/chordeditor/Hint.java
@@ -2,6 +2,7 @@ package com.example.yutauenishi.chordeditor;
 
 
 
+import android.app.AlertDialog;
 import android.content.Intent;
 
 import android.os.Bundle;
@@ -16,6 +17,47 @@ import android.view.View;
 public class Hint extends AppCompatActivity {
     MyOpenHelper helper = new MyOpenHelper(this);
     int id =1;
+
+
+    public void hint0(View v){
+        new AlertDialog.Builder(this)
+                .setTitle("hint0")
+                .setMessage("hint0をクリックしました。")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    public void hint1(View v){
+        new AlertDialog.Builder(this)
+                .setTitle("hint1")
+                .setMessage("hint1をクリックしました。")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    public void hint2(View v){
+        new AlertDialog.Builder(this)
+                .setTitle("hint2")
+                .setMessage("hint2をクリックしました。")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    public void hint3(View v){
+        new AlertDialog.Builder(this)
+                .setTitle("hint3")
+                .setMessage("hint3をクリックしました。")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    public void hint4(View v){
+        new AlertDialog.Builder(this)
+                .setTitle("hint4")
+                .setMessage("hint4をクリックしました。")
+                .setPositiveButton("OK", null)
+                .show();
+    }
 
     // ツールバー
     public void toolbar(String name){
@@ -43,7 +85,7 @@ public class Hint extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hint);
-        toolbar("");
+        toolbar("Chord Editor");
 
 
     }
