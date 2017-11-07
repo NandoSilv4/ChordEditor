d7e5aa9520e22b9dbba6ae2246992f29d924f348
YutaUenishi <yuta915u@gmail.com>
2017-11-07 22:56:29 +0900

    test

--- app/src/main/java/com/sakamalab/yutauenishi/chordeditor/SoundPoolTest.java
+++ app/src/main/java/com/sakamalab/yutauenishi/chordeditor/SoundPoolTest.java
@@ -17,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 
 public class SoundPoolTest extends AppCompatActivity {
@@ -24,7 +25,8 @@ public class SoundPoolTest extends AppCompatActivity {
     SoundPool s_00,s_01,s_02,s_03,s_04,s_05,s_06,s_07,s_08,s_09,s_10,s_11;
     int ID_00,ID_01,ID_02,ID_03,ID_04,ID_05,ID_06,ID_07,ID_08,ID_09,ID_10,ID_11;
     int id =1;
-    HashMap<String,Integer> SoundMap;
+
+    AnalysisChords AC=new AnalysisChords();
     // ツールバー
     public void toolbar(String name,String artist){
         // ツールバーをアクションバーとして使う
@@ -48,23 +50,23 @@ public class SoundPoolTest extends AppCompatActivity {
     }
 
 
-    //曲で使われているすべてのコードの種類と出現回数
+
     public void PlaySound(HashMap<String, Integer> SM){
         for (String ToneName : SM.keySet()) {
             Integer tone = SM.get(ToneName);
             switch (tone){
-                case 0:s_00.play(ID_00, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 1:s_01.play(ID_01, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 2:s_02.play(ID_02, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 3:s_03.play(ID_03, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 4:s_04.play(ID_04, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 5:s_05.play(ID_05, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 6:s_06.play(ID_06, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 7:s_07.play(ID_07, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 8:s_08.play(ID_08, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 9:s_09.play(ID_09, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 10:s_10.play(ID_10, 1.0f, 1.0f, 0, 0, 1.0f);break;
-                case 11:s_11.play(ID_11, 1.0f, 1.0f, 0, 0, 1.0f);break;
+                case 0:s_00.play(ID_00, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 1:s_01.play(ID_01, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 2:s_02.play(ID_02, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 3:s_03.play(ID_03, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 4:s_04.play(ID_04, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 5:s_05.play(ID_05, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 6:s_06.play(ID_06, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 7:s_07.play(ID_07, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 8:s_08.play(ID_08, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 9:s_09.play(ID_09, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 10:s_10.play(ID_10, 0.5f, 0.5f, 0, 0, 1.0f);break;
+                case 11:s_11.play(ID_11, 0.5f, 0.5f, 0, 0, 1.0f);break;
             }
         }
     }
@@ -78,7 +80,18 @@ public class SoundPoolTest extends AppCompatActivity {
     }
 
 
+    int count=0;
+    public void Play(View v) {
 
+        chords= Pattern.compile("\\n").matcher(chords).replaceAll("");
+        String[] data_split = chords.split(",", 0);
+        HashMap<String,Integer> SoundMap;
+        SoundMap=AC.ChordNameAnalysis(data_split[count]);
+        PlaySound(SoundMap);
+        count++;
+    }
+
+    String chords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,43 +106,17 @@ public class SoundPoolTest extends AppCompatActivity {
         boolean mov1 = c.moveToFirst();
 
         String title = c.getString(0);
-        String chords = c.getString(1);
+        chords = c.getString(1);
         String artist = c.getString(2);
         toolbar(title,artist);
 
-        AnalysisChords AC=new AnalysisChords();
 
-        HashMap<String,Integer> map;
-        map=AC.UsedChord(chords);
 
-        String text="";
-        for (String key : map.keySet()) {
-            Integer n_times = map.get(key);
-            text=text+key+"が"+n_times+"回\n";
-        }
+
+
 
         TextView textView = (TextView) findViewById(R.id.text);
-        textView.setText(text);
-
-
-
-        LinearLayout layout = (LinearLayout) this.findViewById(R.id.mainLayout);
-        for (String key : map.keySet()) {
-            SoundMap=AC.ChordNameAnalysis(key);
-
-            Button button = new Button(this);
-            button.setText(key);button.setAllCaps(false);layout.addView(button);
-            // リスナーをボタンに登録
-            button.setOnClickListener(new View.OnClickListener() {
-                //@Override
-                public void onClick(View v) {
-                    AnalysisChords AC=new AnalysisChords();
-
-                    PlaySound(SoundMap);
-                }
-            });
-        }
-
+        textView.setText(chords);
 
         c.close();
         db.close();
